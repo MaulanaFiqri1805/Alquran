384729f0b8f72f54c45fa0f73dce4845df72ef18
Maulana Fiqri Nurul Fawaid <80254752+MaulanaFiqri1805@users.noreply.github.com>
2022-01-30 22:50:17 +0700

    upload
    1.0

---
+++ app/src/main/java/com/fiqri/alquran/model/nearby/ModelGeometry.kt
@@ -0,0 +1,8 @@
+package com.fiqri.alquran.model.nearby
+
+import com.google.gson.annotations.SerializedName
+
+class ModelGeometry {
+    @SerializedName("location")
+    lateinit var modelLocation: ModelLocation
+}
